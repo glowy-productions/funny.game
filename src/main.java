9e63ee687ea09397dd7c5e54905c963d570fdbf3
glowy-productions <glowmedia.info@googlemail.com>
2012-07-24 01:03:17 +0300

    Update src/main.java

--- src/main.java
+++ src/main.java
@@ -1,14 +1,25 @@
-import java.io.IOException;
-
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
-
 public class  main{
-
-	 public static  void main(String[ ] args) throws LineUnavailableException, IOException, UnsupportedAudioFileException
+	 public static  void main(String args[])
      {
+		   //At working is a Debug Screen:D
            // Ausgabe Hello World!
            System.out.println("Hello World!");
            System.out.println("plays");
            System.out.println("EXIT");
-       }}
+       }
+     @SuppressWarnings("unused")
+	public static void rechne(String args[]){
+     int test = 5 + 4;
+     System.out.println("At work");
+     }
+
+
+
+
+
+
+
+
+
+
+}
