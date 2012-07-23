5e7b2ae9e0c797282f7d5cf6c148504c3181ef99
Max <Max@Max-xD>
2012-07-23 23:37:10 +0200

    My Game tests

---
+++ src/main.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class  main{
+
+	 public static  void main(String[ ] args) throws LineUnavailableException, IOException, UnsupportedAudioFileException
+     {
+           // Ausgabe Hello World!
+           System.out.println("Hello World!");
+           System.out.println("plays");
+       }}
